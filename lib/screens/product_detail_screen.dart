db317835d24dbb50ced9b52e5eaea47ad8378524
summaiya14 <summaiyamahmod@gmail.com>
2021-10-14 03:03:59 +0500

    product details addes

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/products.dart';
@@ -12,6 +14,25 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover,),
+            ),
+            SizedBox(height: 10,),
+            Text('\$${loadedProduct.price}', style: TextStyle(fontSize: 20, color: Colors.grey),),
+            SizedBox(height: 10,),
+            Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(loadedProduct.description, textAlign: TextAlign.center, softWrap: true,),),
+          ],
+        ),
+      ),
+
     );
   }
 }
