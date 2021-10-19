eaa2a9d6f93a3872d10cd970f91e56e7a6ade37a
summaiya14 <summaiyamahmod@gmail.com>
2021-10-20 01:11:31 +0500

    manage products screen added

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,19 @@
+import 'package:Shop_App/widgets/app_drawer.dart';
+import 'package:flutter/material.dart';
+
+class EditProductSreen extends StatefulWidget {
+  @override
+  _EditProductSreenState createState() => _EditProductSreenState();
+}
+
+class _EditProductSreenState extends State<EditProductSreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Edit Product"),
+      ),
+      body: ,
+    );
+  }
+}
